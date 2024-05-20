b94a38cf0d3c3a40d82845cbd907508c7748b0f5
vbnk22 <oleszek.patryk@wp.pl>
2024-05-21 01:10:27 +0200

    registerOnCourse in Student class implemented and changed type of index in constructor

--- Student.java
+++ Student.java
@@ -12,7 +12,7 @@
 import java.util.ArrayList;
 
 public class Student extends User {
-	public Student(String fullName, String password, int index) {
+	public Student(String fullName, String password, String index) {
 		super(fullName, password, index);
 	}
 
