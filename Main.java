14723c685bb2de1847360c63856f60e3e1f17ec1
verba <verba447@gmail.com>
2024-05-20 23:03:49 +0200

    comment

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        University university = new University("UMCS");
+        User user;
+        while(true){
+
+        }
+    }
+}
